9fcf8466533973199cfb86cefd7812449908ad97
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-03-27 15:03:53 +0300

    initial commit

---
+++ src/HelloJava.java
@@ -0,0 +1,11 @@
+// примеры решений из книги Философия Java
+
+import java.util.Date;
+public class HelloJava {
+
+    public static void main(String[] args){
+        System.out.println("Привет! Сегодня");
+        System.out.println(new Date());
+    }
+
+}
